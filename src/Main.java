c9bcc2e71e2a48e669d001b1f1a517b6decf7d1a
AntonTupikin <antontupikin@icloud.com>
2023-01-23 12:32:31 +0500

    Second Commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,10 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int[] sales = new int[10];
+        for (int i = 0; i < sales.length; i++) {
+            sales[i] = (int) ((i + 1) * Math.random());
+        }
+        SalesManager salesManager = new SalesManager(sales);
+        System.out.println("Лучшая продажа "+salesManager.max());
     }
 }
