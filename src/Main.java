c1f9bff282b146a7ad349c55bbce70d43fe5d9d2
AntonTupikin <antontupikin@icloud.com>
2023-01-23 17:09:59 +0500

    Firth

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ public class Main {
     public static void main(String[] args) {
         int[] sales = new int[10];
         for (int i = 0; i < sales.length; i++) {
-            sales[i] = (int) ((i + 1) * Math.random()*10);
+            sales[i] = (int) ((i + 1) * Math.random()*100);
         }
         SalesManager salesManager = new SalesManager(sales);
         System.out.println("Лучшая продажа " + salesManager.max());
