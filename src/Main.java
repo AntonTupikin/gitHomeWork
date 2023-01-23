5d09d0d3723223da41455441d177f76f195c9250
AntonTupikin <antontupikin@icloud.com>
2023-01-23 13:17:02 +0500

    Fourth commit

--- src/Main.java
+++ src/Main.java
@@ -2,9 +2,9 @@ public class Main {
     public static void main(String[] args) {
         int[] sales = new int[10];
         for (int i = 0; i < sales.length; i++) {
-            sales[i] = (int) ((i + 1) * Math.random());
+            sales[i] = (int) ((i + 1) * Math.random()*10);
         }
         SalesManager salesManager = new SalesManager(sales);
-        System.out.println("Лучшая продажа "+salesManager.max());
+        System.out.println("Лучшая продажа " + salesManager.max());
     }
 }
